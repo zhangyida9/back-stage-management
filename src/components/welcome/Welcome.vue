444aa37cd5e4c635d1cb8a3dc83457068a3c170e
“zzhangyida9 <1652218453@qq.com>
2021-04-20 23:09:05 +0800

    update welcome note

--- src/components/welcome/Welcome.vue
+++ src/components/welcome/Welcome.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>welcome</div>
+  <div>
+    <div>welcome</div>
+    <br>
+    <div>本系统是基于vue + element ui实现的电商后台管理系统</div>
+    <br>
+    <div>您可以点击左侧菜单栏查看相应的功能</div>
+  </div>
 </template>
 
 <script>
